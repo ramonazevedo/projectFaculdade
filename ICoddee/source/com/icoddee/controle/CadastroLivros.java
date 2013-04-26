6ea587d317e509797cd30d2880e4833838dfbe64
Jocelio Lima <Jocelio27@gmail.com>
2013-04-26 11:42:40 -0300

    commit dos controles

---
+++ ICoddee/source/com/icoddee/controle/CadastroLivros.java
@@ -0,0 +1,12 @@
+package com.icoddee.controle;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CadastroLivros {
+
+	
+	
+}
